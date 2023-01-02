b4c131e4ba884080f00e62120043a7c5d1ded661
CTtao <1838346304@qq.com>
2023-01-02 19:48:09 +0800

    引入Gateway，并整合nacos

---
+++ shop-gateway/src/main/java/com/ct/shop/GatewayStarter.java
@@ -0,0 +1,17 @@
+package com.ct.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author CT
+ * @description
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class GatewayStarter {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayStarter.class,args);
+    }
+}
