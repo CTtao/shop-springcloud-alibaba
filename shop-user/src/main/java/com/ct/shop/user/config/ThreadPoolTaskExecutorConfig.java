a67f417daa8142034f72be5e079a9b4ca98ec0bd
CTtao <1838346304@qq.com>
2023-03-11 16:58:08 +0800

    链路追踪Sleuth：自定义任务线程池

---
+++ shop-user/src/main/java/com/ct/shop/user/config/ThreadPoolTaskExecutorConfig.java
@@ -0,0 +1,28 @@
+package com.ct.shop.user.config;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurerSupport;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+/**
+ * @author CT
+ * @description
+ */
+@Configuration
+@EnableAutoConfiguration
+public class ThreadPoolTaskExecutorConfig extends AsyncConfigurerSupport {
+
+    @Override
+    public Executor getAsyncExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(2);
+        executor.setMaxPoolSize(5);
+        executor.setQueueCapacity(10);
+        executor.setThreadNamePrefix("trace-thread-");
+        executor.initialize();
+        return executor;
+    }
+}
